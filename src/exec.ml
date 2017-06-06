1ed3c616db73e39c74a433cea4beca5729c395e1
Shon Feder <shon.feder@key.me>
2017-06-05 23:02:47 -0400

    Add module for executable end points

---
+++ src/exec.ml
@@ -0,0 +1,4 @@
+module Text_editor_composition = Composition.Make(Text_editor.Prog(Line_editor))
+module Text_editor = Consolate_term.Loop(Text_editor_composition)
+
+module Line_editor = Consolate_term.Loop(Line_editor)
