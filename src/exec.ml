98a25cc26ac495460904aca2ba4432917017e4b4
Shon Feder <shon.feder@key.me>
2017-06-08 15:15:31 -0400

    Implement basic working version of Menu module

--- src/exec.ml
+++ src/exec.ml
@@ -2,3 +2,5 @@ module Text_editor_composition = Composition.Make(Text_editor.Prog(Line_editor))
 module Text_editor = Consolate_term.Loop(Text_editor_composition)
 
 module Line_editor = Consolate_term.Loop(Line_editor)
+
+module Menu_test = Consolate_term.Loop(Menu.Make(Menu.Test))
--- src/menu.ml
+++ src/menu.ml
@@ -20,37 +20,95 @@ struct
       | Next
       | Prev
       | Close
+
+    let option_msg_of_arrow model = function
+      | `Up   -> Some Prev
+      | `Down -> Some Next
+      | _ -> None
+
+    let option_msg_of_key model = function
+      | `Escape -> Some Close
+      | `Enter  -> Some Select
+      | `Arrow dir -> option_msg_of_arrow model dir
+      | _ -> None
+
+    let of_state (event, model )=
+      match event with
+      | `Key (key, _) -> option_msg_of_key model key
+      | _ -> None
   end
 
   module Model =
   struct
-    type t = Msg.t entry Slider.t
+    type item = Msg.t entry
+    type t = item Slider.t
     let of_menu_items = Slider.of_list
+    let selected_msg model =
+      match Slider.select model with
+      | None       -> None
+      | Some entry -> Some entry.msg
+
+    let next model =
+      if Slider.at_last model
+      then model
+      else Slider.fwd model
+
+    let prev = Slider.bwd
+  end
+
+  module Return =
+  struct
+    type t = Msg.t
   end
 
   module Update =
   struct
-    include CT.Make.Update.Types (Model)
+    include CT.Make.Update.Types (Model) (Return)
+
     let init = Model.of_menu_items Msg.menu_items
     (* TODO parse a string in to a menu *)
+
+    let model_of_msg model : Message.t -> return =
+      let open Message in
+      function
+      | Close  -> Error None
+      | Select -> Error (Model.selected_msg model)
+      | Prev   -> Ok (Model.prev model)
+      | Next   -> Ok (Model.next model)
+
     let load _ = init
-    let of_state = raise (Failure "temp")
+    let of_state : state -> return =
+      fun (event, model) ->
+        match Message.of_state (event, model) with
+        | None     -> Ok model
+        | Some msg -> model_of_msg model msg
   end
 
   module View =
   struct
 
-    let sep = I.string A.(fg blue) " | "
-    let of_title title = I.(string A.empty title)
-    let of_item {title; desc} =
-      let description = Option.default "" desc in
-      I.(of_title title <|> sep <|> string A.empty description)
+    let sep bg' = I.string A.(fg blue ++ bg') " | "
+    let of_title bg title = I.(string bg title)
+    let of_desc bg desc = I.string bg (Option.default "" desc)
+
+    let of_entry bg {title; desc} =
+      I.(of_title bg title <|> sep bg <|> of_desc bg desc)
+    let of_unselected : Model.item -> Notty.image = of_entry A.empty
+    let of_selected   : Model.item -> Notty.image = of_entry A.(bg red)
 
     let of_model model =
-      model
-      |> Slider.to_list
-      |> List.map of_item
-      |> I.vcat
+      let above = Slider.front model |> List.map of_unselected in
+      let below = Slider.back  model |> List.map of_unselected in
+      let selected =
+        match Slider.select model with
+        | None       -> I.empty
+        | Some entry -> of_selected entry
+      in
+      I.(vcat above
+         <->
+         selected
+         <->
+         vcat below)
   end
 
 end (* Make *)
