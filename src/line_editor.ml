641eabdd3bb0d9290ce90860e1d0d6fd40cf2b9a
Shon Feder <shon.feder@key.me>
2017-06-12 23:20:36 -0400

    Remove dead code

--- src/line_editor.ml
+++ src/line_editor.ml
@@ -1,45 +1,6 @@
 open Notty
 open Notty_unix
 
-(* module type T = *)
-(* sig *)
-(*   module Model : sig *)
-(*     type t *)
-(*     val empty : t *)
-(*     val is_empty : t -> bool *)
-
-(*     val at_start : t -> bool *)
-(*     val at_end   : t -> bool *)
-
-(*     val to_start : t -> t *)
-(*     val to_end   : t -> t *)
-
-(*     val of_string : string -> t *)
-(*     val to_string : t -> string *)
-
-(*     val to_chars  : t -> Uchar.t list *)
-
-(*     val front    : t -> Uchar.t list *)
-(*     val back     : t -> Uchar.t list *)
-
-(*     val append : t -> t -> t *)
-(*     val split  : t -> t * t *)
-(*   end *)
-
-(*   module Update : sig *)
-(*     type state  = Consolate_term.event * Model.t *)
-(*     type 'a return = (Model.t, 'a) result *)
-(*     val init : Model.t *)
-(*     val load : string -> Model.t *)
-(*     val of_state : state -> 'a return *)
-(*   end *)
-
-(*   module View : sig *)
-(*     val of_model  : Model.t -> Notty.image *)
-(*     val of_uchars : Uchar.t list -> Notty.image *)
-(*   end *)
-(* end *)
-
 module Aux =
 struct
   let drop_last list =
