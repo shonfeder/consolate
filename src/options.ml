466d07785a7effede32ff5a13b1e069c8b05543c
Shon Feder <shon.feder@key.me>
2017-06-21 00:27:19 -0400

    Breadcrumbs mark submenu traversal

--- src/options.ml
+++ src/options.ml
@@ -10,32 +10,35 @@ let find = CharMap.Exceptionless.find
 module type Opts =
 sig
 
-  (* Lvl 5 *)
+  (* Lvl 4 *)
   type name        = string
   type description = string option
   type selector    = char
 
-  (* Lvl 4 *)
+  (* Lvl 3 *)
   type 'a opt =
     { name        : name
     ; thing       : 'a
     ; description : description }
 
-  (* Lvl 3 *)
+  (* Lvl 2 *)
   type 'a opts =
     { title : name
     ; opts  : 'a choice}
+  and 'a choice =
+    'a item CharMap.t
   and 'a item =
     | Opt  of 'a opt
     | Opts of 'a opts
 
-  (* Lvl 2 *)
-  and 'a choice = 'a item CharMap.t
-
   (* Lvl 1 *)
-  type 'a t = 'a choice Slider.t
+  type 'a t = 'a opts
 
-  val of_assoc : (selector * 'a item) list -> 'a choice
+  val opts_items : 'a t -> 'a choice
+  val opts_title : 'a t -> name
+
+  val of_assoc : string -> (selector * 'a item) list -> 'a choice
+  val of_opts  : string -> (selector * 'a item) list -> 'a opts
 
   val opt  : ?description:string -> name -> 'a -> 'a item
   val opts : name -> 'a choice -> 'a item
@@ -67,13 +70,25 @@ struct
   and 'a choice = 'a item CharMap.t
 
   (* Lvl 1 *)
-  type 'a t = 'a choice Slider.t
+  type 'a t = 'a opts
 
-  let of_assoc : (selector * 'a item) list -> 'a choice = fun assoc ->
+  let opts_items : 'a t -> 'a choice
+    = fun opts -> opts.opts
+
+  let opts_title : 'a t -> name
+    = fun opts -> opts.title
+
+  let of_assoc : string -> (selector * 'a item) list -> 'a opts
+    = fun title assoc ->
     let add_item (selector, item) choice =
       CharMap.add selector item choice
     in
-    List.fold_right add_item assoc CharMap.empty
+    let opts = List.fold_right add_item assoc CharMap.empty in
+    {title; opts}
+
+  let of_opts  : string -> (selector * 'a item) list -> 'a item
+    = fun string assoc ->
+      Opts (of_assoc string assoc)
 
   let opt ?description name thing : 'a item =
     Opt {name; thing; description}
@@ -84,15 +99,20 @@ end
 module type Menu_spec =
 sig
   type t
-  val menu : t Opts.choice
+  val menu : t Opts.t
 end
+
+(* TODO Add titles *)
+(* TODO Track bread crumbs of patch through menu tree *)
+(* TODO For menues longer than the screen (pages),
+   make '<' and '>' to go next page *)
 
 module Make (Menu:Menu_spec) : CT.Program with type Return.t = Menu.t =
 struct
   module Model =
   struct
-    type t = Menu.t Opts.t
-    let of_menu : 'a Opts.choice -> t = Slider.singleton
+    type t = Menu.t Opts.t Slider.t
+    let of_menu : 'a Opts.t -> t = Slider.singleton
   end
 
   module Return =
@@ -134,10 +154,11 @@ struct
         let open Opts in
         let update =
           Slider.select model >>=
+          fun opts -> Some (Opts.opts_items opts) >>=
           find selector       >>= function
           | Opt opt   -> Some (Flow.return opt.thing)
           | Opts opts -> Some (Slider.fwd model
-                               |> Slider.insert opts.opts
+                               |> Slider.insert opts
                                |> Flow.cont)
         in
         update |? Flow.cont model
@@ -164,23 +185,30 @@ struct
   struct
     open Opts
 
+    (* auxiliary image functions *)
+    let rec hconcat
+      : ?sep:Notty.image   -> Notty.image list -> Notty.image
+      = fun ?(sep=I.empty) -> function | []    -> I.empty
+                                       | i::is -> I.(i <|> sep <|> hconcat ~sep is)
+
+
     let opt_sep  = I.(string A.(fg blue) " › ")
     let opts_sep = I.(string A.(fg red) " » ")
     let of_name n     = I.(string A.empty n)
     let of_title t    = I.(string A.(fg magenta) t)
     let of_selector s = I.(char A.(fg cyan) s 1 1)
 
-    let of_opt s opt =
+    let of_opt_item s opt =
       I.(of_selector s <|> opt_sep <|> of_name opt.name)
-    let of_opts s opts =
+    let of_opts_item s opts =
       I.(of_selector s <|> opts_sep <|> of_title opts.title)
     let of_item s = function
-      | Opt opt   -> of_opt s opt
-      | Opts opts -> of_opts s opts
+      | Opt opt   -> of_opt_item s opt
+      | Opts opts -> of_opts_item s opts
 
-    let of_choice
-      : ?rows:int -> Menu.t Opts.choice -> Notty.image
-      = fun ?(rows=10) choice ->
+    let of_opts
+      : ?rows:int -> Menu.t Opts.t -> Notty.image
+      = fun ?(rows=10) opts ->
 
         let add_column : Notty.image * Notty.image -> Notty.image
           = fun (column, columns) -> I.(columns <|> pad ~r:2 column)
@@ -196,13 +224,27 @@ struct
             else item_img, add_column (column, columns)
         in
         (I.empty, I.empty)
-        |> CharMap.fold arrange choice
+        |> CharMap.fold arrange opts.opts
         |> add_column
 
-    let of_model : Model.t -> Notty.image = fun model ->
+    let of_model
+      : Model.t -> Notty.image
+      = fun model ->
+        (* TODO Refactor out the bread crumbs *)
+        (* TODO Pick up bread crumbs when back backtracking
+           using Slider.front etc... *)
+        let bread_crumbs =
+          model
+          |> Slider.to_list
+          |> List.map Opts.opts_title
+          |> List.map of_title
+          |> hconcat ~sep:opts_sep
+        in
       match Slider.select model with
-      | None        -> I.empty
-      | Some choice -> of_choice ~rows:1 choice
+      | None      -> I.empty
+      | Some opts -> I.(of_opts ~rows:2 opts
+                        <->
+                        bread_crumbs)
   end
 end (* Make *)
 
@@ -210,11 +252,10 @@ module Test : Menu_spec with type t = int =
 struct
   type t = int
   open Opts
-  let menu = of_assoc
+  let menu = of_assoc "main menu"
       [ 'a', opt "option a" 1
       ; 'b', opt "option b" 2
-      ; 'c', opts "more opts" @@
-        of_assoc
+      ; 'c', of_opts "more opts"
           [ 'x', opt "option x" 3
           ; 'y', opt "option y" 4
           ; 'z', opt "option z" 5
